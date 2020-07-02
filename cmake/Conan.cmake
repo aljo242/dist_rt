256cb7bac24752c5637bfa5dfad3740a68acfc6d
aljo242 <amj92@pitt.edu>
2020-07-02 14:36:16 -0400

    set up

---
+++ cmake/Conan.cmake
@@ -0,0 +1,36 @@
+# All taken from Jason Turner (https://github.com/lefticus) 
+# retyped by me https://github.com/aljo242 for learning
+
+macro(run_conan)
+	# Download automatically, you can also just copy the conan.cmake file
+	if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+		message(STATUS
+			"Downloading conan.cmake from https://github.com/conan-io/cmake-conan"
+			)
+		file(DOWNLOAD
+			"https://github.com/conan-io//cmake-conan/raw/v0.15/conan.cmake"
+			"${CMAKE_BINARY_DIR}/conan.cmake"
+			)
+	endif()
+
+	include(${CMAKE_BINARY_DIR}/conan.cmake)
+
+	conan_add_remote(
+		NAME
+		bincrafters
+		URL
+		https://api.bintray.com/conan/bincrafters/public-conan
+		)
+
+	conan_cmake_run(
+		REQUIRES
+		${CONAN_EXTRA_REQUIRES}
+		spdlog/1.6.1
+		OPTIONS
+		${CONAN_EXTRA_OPTIONS}
+		BASIC_SETUP
+		CMAKE_TARGETS
+		BUILD
+		missing
+		)
+endmacro()
