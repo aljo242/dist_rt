2f5a6f34a92bcd805c3ef4fc056ce7feec2405b1
aljo242 <amj92@pitt.edu>
2020-10-06 19:06:24 -0400

    stb write output functional

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <fstream>
 #include <iostream>
 #include <optional>
+#include <array>
+#include <vector>
 #include <spdlog/spdlog.h>
 #include "glm/glm.hpp"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 
 #include <omp.h>
@@ -13,7 +18,10 @@ using Point3 = glm::vec3;
 using Color3 = glm::vec3;
 
 constexpr float epsilon{ 0.000001f };
-
+constexpr int imageWidth{ 512 };
+constexpr int imageHeight{ 512 };
+constexpr int numChannels{ 3 };
+constexpr int imageSize{ imageHeight * imageWidth * numChannels};
 
 
 class Ray3
@@ -110,6 +118,31 @@ int main()
 		spdlog::critical("nullopt evaluate to true");
 	}
 
+	const char* filename = "test_output.png";
+	
+	std::vector<uint8_t> image(imageSize);
+	size_t index = 0;
+	for (size_t i = 0; i < imageHeight * imageWidth; ++i)
+	{
+		size_t u = i % imageWidth;
+		size_t v = i / imageHeight;
+		//spdlog::critical("({}, {})", u, v);
+		const auto r = static_cast<float>(u) / (imageWidth - 1);
+		const auto g = static_cast<float>(v) / (imageHeight - 1);
+		const auto b = 0.25f;
+
+		const auto ir = static_cast<uint8_t>(255.99f * r);
+		const auto ig = static_cast<uint8_t>(255.99f * g);
+		const auto ib = static_cast<uint8_t>(255.99f * b);
+
+		image[index++] = ir;
+		image[index++] = ig;
+		image[index++] = ib;
+	}
+	
+
+	spdlog::critical("Writing to file: {}", filename);
+	stbi_write_png(filename, imageWidth, imageHeight, numChannels, image.data(), imageWidth*numChannels);
 
 	return 0;
 }
